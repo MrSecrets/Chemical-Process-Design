cd4c28c463b72ac4e37492b38d50c2feda45b09e
MrSecrets <sreerajsj10@hotmail.com>
2021-03-07 18:52:50 +0530

    Optimized graph

--- Task3/level3EP.m
+++ Task3/level3EP.m
@@ -13,7 +13,6 @@ at = [0.0006 0.415 -2.48 5.84 -3.437];
 for i = 6:1:12
     EP3 = [];
     conv = [];
-    M=1/i;
     for x = 0.01:0.01:0.999
 
     ss = as(1) +as(2)*x + as(3)*x*x + as(4)*x*x*x+ as(5)*x*x*x*x;
@@ -28,7 +27,8 @@ for i = 6:1:12
     PT = Peb*st + 0.01*Peb;
     PM = Peb*st;
     PH = PS - PM;
-
+    
+    M = (Peb/x)/(i*Feed);
     Rg = Feed*(1-x)/x;
     CA_cost = (M/x)^0.82;
     CO_cost = M/x;
@@ -37,9 +37,9 @@ for i = 6:1:12
     RA_cost = (M/(x*1))^0.33 * (log(1/(1-x)))^0.33;
 
 
-    EP2 = 23*PS + 9*PB + 8.5*PT + 4*(0.123*PH + 0.383*PM + 0.596*PE) - Feed*16 -M*Feed*0.07 ;
+    EP2 = 23*PS + 9*PB + 8.5*PT + 4*(0.123*PH + 0.383*PM + 0.596*PE) - Feed*16 -i*Feed*0.07 ;
     EP3i = EP2-CA_cost-CO_cost-RA_cost;
-%     EP3i = ;
+%     EP3i = EP2;
     EP3 = [EP3; EP3i];
     conv = [conv; x];
     
@@ -51,7 +51,7 @@ for i = 6:1:12
     
 end 
 
-legend(Legend,'Location','northeast')
+legend(Legend,'Location','southeast')
 title('Economic Potential Level 3 vs Conversion - Ethylbenzene')
 xlabel('Conversion Ethylbenzene')
 ylabel('Economic Potential Level 3: $')
